09a1c953bece989a13799ceef84f61620574c086
renxiaole <wb_renxiaole@meituan.com>
2024-01-17 11:30:57 +0800

    init

---
+++ test-commit-rebase/src/main/java/com/fadaitu/AppMain.java
@@ -0,0 +1,11 @@
+package com.fadaitu;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMain {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
