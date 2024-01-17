446fd0a65929eb0ac65b25b52a57139511f792f5
renxiaole <wb_renxiaole@meituan.com>
2024-01-17 11:35:09 +0800

    002 ~ 添加测试文件

---
+++ test-commit-rebase/src/main/java/com/fadaitu/utils/DateUtil.java
@@ -0,0 +1,13 @@
+package com.fadaitu.utils;
+
+/**
+ * @author wb_renxiaole 2024-01-17
+ * @version 0.1 init
+ */
+public class DateUtil {
+
+    public static void main(String[] args) {
+        System.out.println("test commit rebase");
+    }
+
+}
