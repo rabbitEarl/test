b02ce295e1df09b8384ec2898367d47a8e1bffcd
renxiaole <wb_renxiaole@meituan.com>
2024-01-17 16:27:27 +0800

    007 ~ 添加测试文件 007

---
+++ test-commit-rebase/src/main/java/com/fadaitu/service/TestService.java
@@ -0,0 +1,13 @@
+package com.fadaitu.service;
+
+/**
+ * @author wb_renxiaole 2024-01-17
+ * @version 0.1 init
+ */
+public class TestService {
+
+    public static void main(String[] args) {
+        System.out.println("test service 0");
+    }
+
+}
