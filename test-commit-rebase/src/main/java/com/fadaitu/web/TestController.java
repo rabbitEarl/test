fe36d485176a8ba81e60ccd24d817c3ea9a792ae
renxiaole <wb_renxiaole@meituan.com>
2024-01-17 11:36:34 +0800

    003 ~ 添加测试文件 controller

---
+++ test-commit-rebase/src/main/java/com/fadaitu/web/TestController.java
@@ -0,0 +1,13 @@
+package com.fadaitu.web;
+
+/**
+ * @author wb_renxiaole 2024-01-17
+ * @version 0.1 init
+ */
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("test commit rebase controller");
+    }
+
+}
