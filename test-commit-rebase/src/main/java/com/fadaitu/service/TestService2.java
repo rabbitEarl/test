291989e7c99536fa197bce7b58f4995920cc63ac
renxiaole <wb_renxiaole@meituan.com>
2024-01-22 10:21:40 +0800

    015 ~ 添加测试文件 015

--- test-commit-rebase/src/main/java/com/fadaitu/service/TestService2.java
+++ test-commit-rebase/src/main/java/com/fadaitu/service/TestService2.java
@@ -6,6 +6,6 @@ package com.fadaitu.service;
  */
 public class TestService2 {
     public static void main(String[] args) {
-        System.out.println("123456");
+        System.out.println("12345699");
     }
 }
